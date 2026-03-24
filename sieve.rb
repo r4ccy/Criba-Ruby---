1237ece13bd623270b6557769d1063007f2b470f
Adriana <adrianasotojimenez3@gmail.com>
2026-03-24 11:50:51 -0400

    feat(master): crear estructura de clase para la criba

--- sieve.rb
+++ sieve.rb
@@ -1,4 +1,15 @@
+class EratosthenesSieve
+  attr_reader :limit, :primes
+
+  def initialize(limit)
+    @limit = limit
+    @primes = []
+  end
+end
+
 puts "Ingrese la cantidad:"
 n = gets.to_i
 
-puts "Número ingresado: #{n}"
+sieve = EratosthenesSieve.new(n)
+puts sieve.limit
+
