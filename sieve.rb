a4d7f6d91e6ed035241b11153c0ea6d0d648aa96
Adriana <adrianasotojimenez3@gmail.com>
2026-03-24 12:09:04 -0400

    feat(master): implementar criba de Eratóstenes

--- sieve.rb
+++ sieve.rb
@@ -5,11 +5,25 @@ class EratosthenesSieve
     @limit = limit
     @primes = []
   end
+
+  def run
+    sieve = Array.new(@limit + 1, true)
+    sieve[0] = sieve[1] = false
+
+    (2..Math.sqrt(@limit)).each do |i|
+      next unless sieve[i]
+      (i * i).step(@limit, i) { |j| sieve[j] = false }
+    end
+
+    @primes = sieve.each_index.select { |i| sieve[i] }
+  end
 end
 
 puts "Ingrese la cantidad:"
 n = gets.to_i
 
 sieve = EratosthenesSieve.new(n)
-puts sieve.limit
+sieve.run
+
+puts sieve.primes.join(', ')
 
